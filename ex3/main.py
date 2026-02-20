c02f45680713b2631d9e28109641d775c069e5e4
mohcine_mac <aittalebmohcine@gmail.com>
2026-02-20 13:19:53 +0000

    ex3 files added

---
+++ ex3/main.py
@@ -0,0 +1,18 @@
+#   === DataDeck Game Engine ===
+#
+#   Configuring Fantasy Card Game...
+#   Factory: FantasyCardFactory
+#   Strategy: AggressiveStrategy
+#   Available types: {'creatures': ['dragon', 'goblin'], 'spells': ['fireball'], 'artifacts': ['mana_ring']}
+#
+#   Simulating aggressive turn...
+#   Hand: [Fire Dragon (5), Goblin Warrior (2), Lightning Bolt (3)]
+#
+#   Turn execution:
+#   Strategy: AggressiveStrategy
+#   Actions: {'cards_played': ['Goblin Warrior', 'Lightning Bolt'], 'mana_used': 5, 'targets_attacked': ['Enemy Player'], 'damage_dealt': 8}
+#
+#   Game Report:
+#   {'turns_simulated': 1, 'strategy_used': 'AggressiveStrategy', 'total_damage': 8, 'cards_created': 3}
+#
+#   Abstract Factory + Strategy Pattern: Maximum flexibility achieved!
